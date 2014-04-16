61876403af957df720b5d27c31d4dc7207579b90
stephanos <sgt29@cornell.edu>
2014-04-15 20:36:35 -0400

    Updated dist files.

--- dist/scheme.js
+++ dist/scheme.js
@@ -5458,11 +5458,13 @@ wb.l10nHalfDone = l10nHalfDone;
     var bscheme = new BiwaScheme.Interpreter(function(e, state) {
         document.querySelector('.stageframe').contentWindow.document.write(e.message);
     });
+    //You could run your SchemeLibrary.lisp file right here and all functions
+    //will be accessible
     wb.runScript = function(script){
         var run = function(){
             wb.script = script;
             var scriptArray = script.split(";;end");
-            
+            document.querySelector('.stageframe').contentWindow.document.body.innerHTML = '';  
             for(var i = 0; i < scriptArray.length; i++) {
                 console.log('THIS IS IMPORTANT:' + scriptArray[i]);
                 bscheme.evaluate(scriptArray[i], function(result) {
