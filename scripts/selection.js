95b0347a4e159e5d9bf9a5807e40ce6472bbe2fa
Wai Ho Leung <whleung@live.com>
2014-07-28 13:26:27 -0700

    Added all except select function in selection.js

--- scripts/selection.js
+++ scripts/selection.js
@@ -1,4 +1,7 @@
 (function(wb){
+    var selected = [];
+    var ascending = false;
+
     /**
      * Selects one particular block or a range of blocks.
      *
@@ -16,12 +19,23 @@
      * Deselects all selected blocks.
      */
     function deselectAll() {
+        while (selected.length > 0) {
+            selected.pop().classList.remove("selected");
+        }
     }
 
     /**
      * Delete all selected blocks from the script.
      */
     function deleteAll() {
+        for (var i = 0; i < selected.length; i++) {
+            selected[i].classList.remove('selected');
+            Event.trigger(selected[i], 'wb-remove');
+            Event.trigger(selected[i], 'wb-delete');
+            selected[i].parentElement.removeChild(selected[i]);
+        }
+
+        selected.length = 0;
     }
 
     /**
@@ -29,12 +43,23 @@
      * the cloned blocks.
      */
     function clone() {
+        var div = wb.elem('div');
+
+        for (var i = 0; i < selected.length; i++) {
+            var index = ascending ? i : selected.length - 1 - i;
+            var clonedBlock = wb.block.clone(selected[index]);
+            Event.trigger(selected[index], 'wb-clone');
+            div.appendChild(clonedBlock);
+        }
+
+        return div;
     }
 
     /**
      * Returns an array of selected blocks.
      */
     function get() {
+        return selected;
     }
 
     /**
@@ -42,12 +67,14 @@
      * the selecting order.
      */
     function getFirst() {
+        return ascending ? selected[0] : selected[selected.length - 1];
     }
 
     /**
      * Returns the first selected block.
      */
     function getFirstSelected() {
+        return selected[0];
     }
 
     wb.selection = {
